9eafb32df9991266cbf18292db0747a6574686fc
dandean <danwdean@gmail.com>
2010-01-18 13:00:44 -0800

    Add support for function callbacks

--- enter-click.js
+++ enter-click.js
@@ -1,36 +1,62 @@
-/*
- * EnterClick listens on a containing element for the enter/return key being pressed.
- * When the enter key is pressed within a child input element that will submit the form, 
- * it will prevent the default form submittal and fire the click event of a designated button.
+/**
+ *  class EnterClick
+ *
+ *  [[EnterClick]] listens on a containing element for the enter/return key
+ *  being pressed. When the enter key is pressed within a child input element
+ *  that will submit the form, it will prevent the default form submittal and
+ *  fire `callback`.
  *  
- * Usage:
- * $('fieldset_forgot').onEnterClick($('btn_Forgot'));
- */
+ *  Usage:
+ *  $('fieldset_forgot').onEnterClick($('btn_Forgot'));
+**/
 var EnterClick = Class.create({
 	relevantTypes: $w('text password checkbox radio'),
-	initialize: function(element, button){
+
+	/**
+	 *  new EnterClick(@element, callback);
+	**/
+	initialize: function(element, callback) {
 		this.element = $(element).addClassName("enterclick");
-		this.button = $(button);
-		this.element.observe('keypress', this.__keyPress.bindAsEventListener(this));
-	},
-	__keyPress: function(e){
-		var elSrc = e.element();
-		if((e.keyCode == Event.KEY_RETURN) 
-			&& (elSrc.nodeName == 'INPUT') 
-			&& (this.relevantTypes.include(elSrc.type))) {
-				// prevent default form submittal
-				e.stop(); 
-				// fire associated button's click event
-				this.button.click();
+
+		if (Object.isFunction(callback)) {
+			this.callback = callback;
+		} else if (Object.isElement(callback)) {
+			var name = callback.nodeName,
+			    type = callback.type;
+			if (name == "BUTTON" || (name == "INPUT" && (type == "submit" || type == "button"))) {
+				this.callback = callback;
+			}
 		}
-	}
+		if (!this.callback) throw new Error("Invalid callback");
+
+		this.element.observe('keypress', function(e) {
+			var src = e.element();
+			if((e.keyCode == Event.KEY_RETURN) 
+				&& (src.nodeName == 'INPUT') 
+				&& (this.relevantTypes.include(src.type))) {
+					e.stop(); 
+					if (Object.isFunction(this.callback)) {
+						this.callback(e);
+					} else this.button.click();
+			}
+		}.bindAsEventListener(this));
+	},
+	toString: function() { return "[object EnterClick]"; }
 });
-// Extend Element with enterclick
+
 Element.addMethods({
-	onEnterClick: function(element, button) {
-		new EnterClick(element, button);
+	/**
+	 *  Element.onEnterClick(@element, callback) -> Element
+	 *  - callback (Element|Function): The button to click or function to call when the "Enter" key is pressed.
+	 *
+	 *  When the "ENTER" key is pressed while focused on any child text,
+	 *  password, checkbox or radio inputs, the `callback` is invoked. If
+	 *  `callback` is a button or input of type `submit` or `button`, its
+	 *  `click` method is invoked. If `callback` is a [[Function]], it is
+	 *  invoked and the Key event is passed as it's argument.
+	**/
+	onEnterClick: function(element, callback) {
+		new EnterClick(element, callback);
 		return element;
 	}
 });
-
-
