7eb42ed140c7bb0eb791f19ea28300be41eba432
dandean <danwdean@gmail.com>
2010-02-27 14:46:36 -0800

    New Guid class and associated unit tests.

---
+++ tests/guid/test.js
@@ -0,0 +1,42 @@
+Evidence.TestCase.extend('GUID Tests', {
+
+  testGuidAPI: function(t){
+    // Check validity of Guid.EMPTY
+    t.assert(Guid.isGuid(Guid.EMPTY));
+
+    // Check validity of generated Guid
+    t.assert(Guid.isGuid(Guid.create()));
+
+    // Check uniqueness of generated GUIDs.
+    // Testing pool of 50,000 generations
+    var guids = {}, i = 0;
+
+    while (i < 50000) {
+      var guid = Guid.create().toString();
+      guids[guid] = true;
+      i++;
+    }
+    t.assertEqual(Object.keys(guids).length, 50000);
+    
+    // Check that invalid guid string can't be used for instantiation.
+    
+    // I'd rather have:
+    // t.assertError(new Guid("invalid-string"));
+    // than this:
+    try {
+      new Guid("invalid-string");
+      t.assert(false);
+    } catch(e) {
+      t.assert(true);
+    }
+    
+    var guid = Guid.create();
+    
+    // Test basic equality with Guid.EMPTY
+    t.assert(new Guid(Guid.EMPTY).equals(Guid.EMPTY));
+    
+    // Test equality with generated Guid
+    t.assert(guid.equals(guid));
+    t.assert(guid.equals(guid.toString()));
+  }
+});
