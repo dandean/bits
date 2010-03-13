f05378f70b643a19c610c47bebe9f4d5522038f4
dandean <danwdean@gmail.com>
2010-03-13 15:31:14 -0800

    Add PDoc for Element.toHTML. Update test case for new structure.

--- dom/dom.js
+++ dom/dom.js
@@ -1,8 +1,18 @@
-(function(doc) {
+/**
+ *  Element.toHTML(@element) -> String
+ *  - element (Element): The element to transform into an HTML string.
+ *
+ *  Returns the HTML string for the [[Element]]. Used as a means of providing
+ *  a cross-browser `Element#outerHTML` alternative.
+**/
+Element.toHTML = (function(doc) {
+  // Use native outerHTML if present.
+  // Thanks to @jdalton for the collaboration on this.
   var toHTML = function toHTML(element) {
     return $(element).outerHTML;
   };
 
+  // Roll our own version of outerHTML.
   if (!('outerHTML' in doc.documentElement)) {
     var dummy = doc.createElement('html');
     toHTML = function toHTML(element) {
@@ -12,5 +22,6 @@
       return result;
     };
   }
-  Element.addMethods({ 'toHTML': toHTML });
 })(document);
+
+Element.addMethods({ 'toHTML': Element.toHTML });
