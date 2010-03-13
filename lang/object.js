30478d90c6dc0aaec16722dbd885bd8f59197b5c
dandean <danwdean@gmail.com>
2010-03-13 15:36:56 -0800

    New Object file. Currently contains `Object.isDate` method.

---
+++ lang/object.js
@@ -0,0 +1,11 @@
+if (!("isDate") in Object) {
+  /**
+   *  Object.isDate(object) -> Boolean
+   *  - object (Object): An object to test.
+   *
+   *  Returns `true` if `object` is an instance of [[Date]].
+  **/
+  Object.isDate = function(object) {
+    return object instanceof Date;
+  };
+}
