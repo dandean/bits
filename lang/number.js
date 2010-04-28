a6e63214ca13454ed00df38ffa0083f3df8b5608
dandean <danwdean@gmail.com>
2010-04-28 12:44:27 -0700

    Added source reference

--- lang/number.js
+++ lang/number.js
@@ -6,5 +6,6 @@
  *  Returns the `min` or `max` value if the number is outside the specified range.
 **/
 Number.prototype.clamp = function(min, max) {
+  // Via http://twitter.com/kangax/status/13023629344
   return this < min ? min : (this > max ? max : this);
 };
